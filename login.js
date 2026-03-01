358819f0145883d74959925d0e36dea4be661b61
Nikoloz_22 <K0K0L0z1922@gmail.com>
2026-03-01 16:54:53 +0400

    Update login.js

--- login.js
+++ login.js
@@ -61,9 +61,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
-const burgerBtn = document.getElementById("burgerBtn");
-const navBar = document.getElementById("Nav-Bar");
 
-burgerBtn.addEventListener("click", () => {
-  navBar.classList.toggle("show");
-});
