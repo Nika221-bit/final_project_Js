d6efdfe125c150947e295a1417bf275829c60b1c
Nikoloz_22 <K0K0L0z1922@gmail.com>
2026-03-02 20:56:50 +0400

    forgoten pus

--- hotels.js
+++ hotels.js
@@ -24,6 +24,7 @@ fetch('https://hotelbooking.stepprojects.ge/api/Hotels/GetAll')
        roomsContainer.appendChild(card);
    })
 });
+
 const burgerBtn = document.getElementById("burgerBtn");
 const navBar = document.getElementById("Nav-Bar");
 
